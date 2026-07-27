cd99c3ca7304e184c37d388b5133a98c0c21cdf2
eunhye <eunhye@eunhyes-MacBook-Air.local>
2026-07-27 17:41:46 +1000

    Implement list operations

---
+++ src/ToDoList.java
@@ -0,0 +1,11 @@
+public class ToDoList {
+    private User user;
+
+    public ToDoList(User user) {
+        this.user = user;
+    }
+
+    public void run() {
+        System.out.println("To-do list started.");
+    }
+}
